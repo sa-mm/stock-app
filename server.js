c84fd16fc9ceb6a752131166cecf0081451b7fee
Sam <mcmyler@gmail.com>
2017-09-12 12:11:31 -0500

    Start work with new api

--- server.js
+++ server.js
@@ -17,9 +17,33 @@ const allowCrossDomain = function (req, res, next) {
   }
 }
 
+console.log(process.env.ALPHA_VANTAGE_KEY)
+
 app.use(express.static(path.join(__dirname, 'client/build')))
 // app.use(allowCrossDomain)
 
+app.get('/avapi/:string', function (req, res) {
+  const string = req.params.string
+
+  const key = process.env.ALPHA_VANTAGE_KEY
+  const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=MSFT&apikey=' + key
+
+  request.get({
+    url: url,
+    json: true,
+    headers: { 'User-Agent': 'request' }
+  }, (err, response, data) => {
+    if (err) {
+      console.log('Error:', err)
+    } else if (response.statusCode !== 200) {
+      console.log('Status:', response.statusCode)
+    } else {
+      // data is already parsed as JSON:
+      // console.log(data)
+      res.json(data)
+    }
+  })
+})
 app.get('/api/:symbol', function (req, res) {
   const symbol = req.params.symbol
   // console.log(symbol)
